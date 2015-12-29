939c314273d701d2b6253977b5ae58d8c463a800
oanhcodes <linda.oanh.ho@gmail.com>
2015-12-28 18:24:21 -0600

    Add files

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,10 @@
+# Define an Empty Method
+def do_something(name, age, favorite_city)
+	puts name
+	puts age.to_s #age has been converted to a string
+	puts favorite_city
+end
+
+# I worked on this challenge [by myself].
+
+# Your Solution Below
