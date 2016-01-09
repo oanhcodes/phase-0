5ee2bf7be93d5f35541b27dd990849ff0a908003
oanhcodes <linda.oanh.ho@gmail.com>
2016-01-08 21:54:51 -0600

    Add solution and reflection for solo challenge

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -83,16 +83,26 @@ end
 
 # 3. Reflection
 =begin
-#What was your process for breaking the problem down? What different approaches did you consider?
+What was your process for breaking the problem down? What different approaches did you consider?
+
+I broke up the problem by thinking about: 
+1)How can I separate the digits in integer?
+2)How many numbers make up the integer?
+3)Integers with less than 4 numbers do no need a comma
+4)Integers with more than 4 numbers need commas every 3 numbers from the end
+
+I decided to approach the problem by converting the integer into an array of split strings. After getting the length of the integer, I was able to create a statement that returned the original integer as a string if the length was less than 4. I first tested out add commas by using the insert method to add a "," at the -4 index. Since integers with more than 7 numbers need two or more commas, I knew that I would need to use an iterator. I first tried the loop method but settled on the times iterator since I figure out how many commas every integer needed based on the length/3.
 
 Was your pseudocode effective in helping you build a successful initial solution?
+Yes, my pseudocode was very effective in helping me breakdown the problem. Even though I spent a lot of time trying different iterations, it would have taken a lot longer if I didn't know what conditions I needed to have for my code blocks.
 
 What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
+Reverse was the new method I used. This simplified my code because I was able to add commas starting from the 3 index regardless of the length of the integer. I still see a lot of redundancies in my refactored code, however, I wanted to keep the solution to still resemble my intial solution. 
 
 How did you initially iterate through the data structure?
-I originally attempted to iterate with a loop that would break when the index was equal to or greater than the length of the number. This failed because the insertion of commas stopped early with large numbers. The length of integer is not an accurate break point for the loop since each comma increases the length of the entire string. After I tried this, I decided that the times iterator would work better for this solution because I can determine the number of commas each integer would need by dividing the integer by 3. It was just
-
+I originally attempted to iterate with a loop that would break when the index was equal to or greater than the length of the number. This failed because the insertion of commas stopped early with large numbers since. The length of integer was not an accurate break point for the loop since each added comma increased the length of the entire array. After I tried this, I decided that the times iterator would work better for this solution because I can determine the number of commas each integer would need by dividing the integer by 3.
 
 Do you feel your refactored solution is more readable than your initial solution? Why?
+I do think my refactored solution is more readable because it is more concise. The code block in the times iterator is redundant, however, I was unable to clean it up further without running errors.
 
 =end
