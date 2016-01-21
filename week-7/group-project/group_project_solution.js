4dd8dc15c49617787767f7e583bf80eff667051f
oanhcodes <linda.oanh.ho@gmail.com>
2016-01-21 07:22:49 -0600

    Close comment

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -3,3 +3,5 @@
 1. As a user, I want the get the sum for a list of numbers.
 2. As a user, I want to get the average for a list of numbers.
 3. As a user, I want to find middle number in a list of numbers.
+
+*/
