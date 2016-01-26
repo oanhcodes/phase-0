ff3d5351187534e78084cd9ef281c21c3b279ed3
oanhcodes <linda.oanh.ho@gmail.com>
2016-01-26 07:41:48 -0600

    Add refactored solutions to the group summary

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -8,7 +8,39 @@
 
 /* Summary
 
-The code passed all the tests. Together we created successful functions to find
+The code passed all the tests. Together we created successful functions to find 
 the sum, mean, and median of an array! We did what the user requested. Way to go!
 
 */
+
+//Refacoring : Person 5
+//As a user, I want to create a function that accept a list of numbers and add it up.
+function sum(array) {
+var total = 0;
+for (var i = 0; i < array.length; i++){
+  total += array[i];
+}
+return total;
+};
+
+//As a user, I want to create a function that will give us the average of a list of numbers.
+function mean(array) {
+var total = 0;
+  total += array[i];
+}
+return total/(array.length);
+}
+
+//As a user, I want to create a function that will give us the median of numbers from a list
+function median(array) {
+function sortNumber(a,b) {
+  return a - b;
+}
+var len = array.length;
+if (array.length % 2 === 0) {
+  return (array_sort[(len - 2)/2] + array_sort[(len)/2]) / 2;
+}
+else {
+  return array_sort[(len - 1)/2];
+}
+}
