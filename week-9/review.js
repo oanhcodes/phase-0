f4fe689d1963d7b09b6fb756098d6ee6c3496b4d
oanhcodes <linda.oanh.ho@gmail.com>
2016-02-01 19:35:52 -0600

    Add user stories and pseudocode

---
+++ week-9/review.js
@@ -0,0 +1,20 @@
+
+//Release 1: User Stories
+/*
+1. As a user, I want to create a grocery list.
+2. As a user, I want to be able to add items with a specific quantity to my grocery list.
+3. As a user, I want to be able to remove items from my grocery list.
+4. As a user, I want to be able to update the quantity number for items on my grocery list.
+5. As a user, I want my list to be clean and easy to read.
+*/
+
+
+// Release 2: Pseudocode
+/*
+-Create a function that will add properties and values to a object named grocery list
+  -Every item should have a default value of 1 unless otherwise
+-Create a function to insert new items and quantity amount to the grocery list
+-Create a function to remove items and quantity amount from grocery list
+-Create a function that will update quantity of item on list
+-Create a function that will print out a clean list
+*/
