732940ef4bafd1796dc99dc9814468578fdd3068
oanhcodes <linda.oanh.ho@gmail.com>
2016-02-02 14:47:46 -0600

    Add completed challenge

--- week-9/dom-manipulation/home_page.js
+++ week-9/dom-manipulation/home_page.js
@@ -10,25 +10,49 @@
 
 
 
-
 // Release 1:
 
+function release_one() {
+ document.getElementById("release-0").className = "done";
+}
 
 
 
 // Release 2:
 
-
+function release_two() {
+  document.getElementById("release-1").style.display = "none";
+}
 
 
 // Release 3:
 
-
+function release_three() {
+   document.getElementById("release-2").innerHTML = "I completed released 2!";
+}
 
 
 // Release 4:
 
+function release_four() {
+   document.getElementById("release-3").style.backgroundColor = "#955251 ";
+}
 
 
 
 // Release 5:
+function release_five() {
+   var elements = document.getElementsByClassName("release-4");
+   for (var i=0; i < elements.length; i++) {
+   var element = elements[i]
+   element.style.fontSize = "2em";    
+   }
+   
+}
+
+// Release 6:
+
+function release_six() {
+    var tmpl = document.getElementById('hidden');
+    document.body.appendChild(tmpl.content.cloneNode(true));
+}
